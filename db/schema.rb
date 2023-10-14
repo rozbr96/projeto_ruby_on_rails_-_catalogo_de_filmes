26c3572c4aff735d1a63d4136091489cd177fe78
Rosemilson <rozbr96@gmail.com>
2023-10-14 17:31:40 -0300

    Create countries model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_14_203022) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_14_203119) do
+  create_table "countries", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_countries_on_name", unique: true
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
