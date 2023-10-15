367d2756579e69e87af1518f8c409ce93846b19f
Rosemilson <rozbr96@gmail.com>
2023-10-15 13:05:09 -0300

    Add db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+File.open "db#{File::SEPARATOR}countries_nationalities.txt" do |file|
+  file.readlines.each do |line|
+    country_name, nationality_name = line.chomp('\n').split(' - ')
+
+    country = Country.find_or_create_by(name: country_name)
+    Nationality.find_or_create_by(name: nationality_name, country: country)
+  end
+end
