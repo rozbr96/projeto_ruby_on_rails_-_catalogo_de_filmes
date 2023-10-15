77780652d03529e64a2e956eaa4ab42d9e4e7bab
Rosemilson <rozbr96@gmail.com>
2023-10-15 13:23:54 -0300

    Improve home page by adding a minimalist dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   def home
+    @data = [
+      [ 'Filmes', Movie.count ],
+      [ 'Gêneros', Genre.count ],
+      [ 'Diretores', Director.count ],
+      [ 'Países', Country.count ],
+      [ 'Nacionalidades', Nationality.count ]
+    ]
   end
 end
