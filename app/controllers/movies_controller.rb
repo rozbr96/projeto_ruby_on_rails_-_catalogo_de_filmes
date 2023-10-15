3716a66b5c8656440f0fe8ef5eec5e287fde0eb3
Rosemilson <rozbr96@gmail.com>
2023-10-15 00:45:02 -0300

    Refactor movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,7 @@
 class MoviesController < ApplicationController
   def create
-    data = params.require(:movie).permit!
-
-    country = Country.find params[:origin_country_id]
-    director = Director.find params[:director_id]
-    genre = Genre.find params[:genre_id]
-
     begin
-      Movie.create!(
-        title: data[:title], length: data[:length],
-        synopsis: data[:synopsis], release_year: data[:release_year],
-        genre: genre, director: director, origin_country: country
-      )
+      Movie.create get_form_data
 
       redirect_to movies_path
     rescue
@@ -21,9 +11,22 @@ class MoviesController < ApplicationController
 
   def edit
     @movie = Movie.find params[:id]
-    @genres = Genre.all.map { |genre| [genre.name, genre.id] }
-    @directors = Director.all.map { |director| [director.name, director.id] }
-    @countries = Country.all.map { |country| [country.name, country.id] }
+    prepare_form_selects_values
+  end
+
+  def get_form_data
+    data = params.require(:movie).permit!
+
+    country = Country.find params[:origin_country_id]
+    director = Director.find params[:director_id]
+    genre = Genre.find params[:genre_id]
+
+
+    {
+      title: data[:title], length: data[:length],
+      synopsis: data[:synopsis], release_year: data[:release_year],
+      genre: genre, director: director, origin_country: country
+    }
   end
 
   def index
@@ -31,25 +34,18 @@ class MoviesController < ApplicationController
   end
 
   def new
+    prepare_form_selects_values
+  end
+
+  def prepare_form_selects_values
     @genres = Genre.all.map { |genre| [genre.name, genre.id] }
     @directors = Director.all.map { |director| [director.name, director.id] }
     @countries = Country.all.map { |country| [country.name, country.id] }
   end
 
   def update
-    data = params.require(:movie).permit!
-
-    movie = Movie.find params[:id]
-    country = Country.find params[:origin_country_id]
-    director = Director.find params[:director_id]
-    genre = Genre.find params[:genre_id]
-
     begin
-      movie.update!(
-        title: data[:title], length: data[:length],
-        synopsis: data[:synopsis], release_year: data[:release_year],
-        genre: genre, director: director, origin_country: country
-      )
+      Movie.find(params[:id]).update get_form_data
 
       redirect_to movies_path
     rescue
