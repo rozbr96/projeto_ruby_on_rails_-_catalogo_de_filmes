48a1e3b65ec3bebed3dab1f2b6069a63200bcbe4
Rosemilson <rozbr96@gmail.com>
2023-10-14 23:29:30 -0300

    Implement genre creation and edition

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,5 +1,32 @@
 class GenresController < ApplicationController
+  def create
+    begin
+      Genre.create! params.require(:genre).permit(:name)
+      redirect_to genres_path
+    rescue
+      render :new
+    end
+  end
+
+  def edit
+    @genre = Genre.find params[:id]
+  end
+
   def index
     @genres = Genre.all
+  end
+
+  def new
+  end
+
+  def update
+    genre = Genre.find params[:id]
+
+    begin
+      genre.update params.require(:genre).permit(:name)
+      redirect_to genres_path
+    rescue
+      redirect_to :action => :edit, :id => params[:id]
+    end
   end
 end
